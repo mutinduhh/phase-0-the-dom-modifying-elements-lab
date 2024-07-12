3742e42997968ef9437f07ea034d35ece3e00b4a
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-07-12 12:34:19 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+var mainElement = document.querySelector('main');
+if (mainElement) {mainElement.parentNode.removeChild(mainElement);} 
+const newHeader=document.createElement('h1');
+newHeader.id='victory';
+newHeader.innerHTML='Jonathan Mutinda is the champion';
